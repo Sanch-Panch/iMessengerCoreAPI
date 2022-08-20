2c6146df782cc15e35a4fa7e3c857b638b2914c7
Sanch-Panch <111037033+Sanch-Panch@users.noreply.github.com>
2022-08-20 11:20:57 +0300

    Update iMessengerCoreAPIController.cs

--- iMessengerCoreAPI/Controllers/iMessengerCoreAPIController.cs
+++ iMessengerCoreAPI/Controllers/iMessengerCoreAPIController.cs
@@ -1,4 +1,4 @@
-﻿using iMessengerCoreAPI.Models;
+using iMessengerCoreAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iMessengerCoreAPI.Controllers
@@ -9,50 +9,29 @@ namespace iMessengerCoreAPI.Controllers
     {
 
         [HttpGet]
-
         public ActionResult Get([FromQuery] List<Guid> clients)
         {
-
             RGDialogsClients rGDialogsClients = new RGDialogsClients();
             List<RGDialogsClients> information = rGDialogsClients.Init();
-            var dialogs_temp = new Dictionary<Guid, int>();
-            var dialogs_result = new Dictionary<Guid, List<Guid>>();
-            List<Guid> temp = new List<Guid>();
             var makeAllZeroGuID = new Guid();
-            List<Guid> client;
             Guid result = new Guid();
 
-            foreach (RGDialogsClients count in information)
+            var dictionary = new Dictionary<Guid, List<Guid>>();
+
+            for (int i = 0; i < information.Count; i++)
             {
-                temp.Add(count.IDClient);
-                if (dialogs_temp.ContainsKey(count.IDRGDialog)) dialogs_temp[count.IDRGDialog]++;
-                else dialogs_temp.Add(count.IDRGDialog, 1);
+                var key = information[i].IDRGDialog;
+                if (!dictionary.ContainsKey(key)) dictionary[key] = new List<Guid>();
+                dictionary[key].Add(information[i].IDClient);
             }
 
-            int idt = 0;
-            int mng = 0;
-            foreach (var person in dialogs_temp)
+            foreach (var dialog in dictionary)
             {
-                List<Guid> temp_result = new List<Guid>();
-                mng = mng + person.Value;
-                for (int i = idt; i < mng; i++)
+                if (clients.SequenceEqual(dialog.Value))
                 {
-                    temp_result.Add(temp[i]);
-                    idt++;
-                }
-                dialogs_result.Add(person.Key, temp_result);
-            }
-
-            foreach (var person_result in dialogs_result)
-            {
-                if (dialogs_result.TryGetValue(person_result.Key, out client))
-                {
-                    if (clients.SequenceEqual(client))
-                    {
-                        result = person_result.Key;
-                        Console.WriteLine(person_result.Key);
-                        return Ok(person_result.Key);
-                    }
+                    result = dialog.Key;
+                    Console.WriteLine(dialog.Key);
+                    return Ok(dialog.Key);
                 }
             }
             if (result.CompareTo(makeAllZeroGuID) == 0)
